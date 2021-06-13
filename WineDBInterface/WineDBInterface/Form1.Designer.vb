b5b3f39743aed11acb0aca9f602c472910313c30
TheScorpoi <sobral@ua.pt>
2021-06-13 01:25:00 +0100

    update tables

--- WineDBInterface/WineDBInterface/Form1.Designer.vb
+++ WineDBInterface/WineDBInterface/Form1.Designer.vb
@@ -22,11 +22,14 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
         Me.Button1 = New System.Windows.Forms.Button()
         Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.TextBox2 = New System.Windows.Forms.TextBox()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Button1
@@ -42,6 +45,7 @@ Partial Class Form1
         '
         Me.TextBox1.Location = New System.Drawing.Point(244, 171)
         Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.PlaceholderText = "User"
         Me.TextBox1.Size = New System.Drawing.Size(286, 27)
         Me.TextBox1.TabIndex = 1
         '
@@ -49,6 +53,8 @@ Partial Class Form1
         '
         Me.TextBox2.Location = New System.Drawing.Point(244, 228)
         Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.TextBox2.PlaceholderText = "Password"
         Me.TextBox2.Size = New System.Drawing.Size(286, 27)
         Me.TextBox2.TabIndex = 2
         '
@@ -70,11 +76,22 @@ Partial Class Form1
         Me.Label2.TabIndex = 4
         Me.Label2.Text = "PASSWORD"
         '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(316, 27)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(131, 118)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 5
+        Me.PictureBox1.TabStop = False
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.PictureBox1)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.TextBox2)
@@ -82,6 +99,7 @@ Partial Class Form1
         Me.Controls.Add(Me.Button1)
         Me.Name = "Form1"
         Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -92,4 +110,5 @@ Partial Class Form1
     Friend WithEvents TextBox2 As TextBox
     Friend WithEvents Label1 As Label
     Friend WithEvents Label2 As Label
+    Friend WithEvents PictureBox1 As PictureBox
 End Class
--- WineDBInterface/WineDBInterface/Form1.vb
+++ WineDBInterface/WineDBInterface/Form1.vb
@@ -6,4 +6,8 @@
     Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
 
     End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+
+    End Sub
 End Class
