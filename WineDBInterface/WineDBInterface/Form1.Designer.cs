a0b5ea6f1f8fd74da254ca458fd50ef5e2066b97
TheScorpoi <sobral@ua.pt>
2021-06-16 15:05:00 +0100

    connection to DB

--- WineDBInterface/WineDBInterface/Form1.Designer.cs
+++ WineDBInterface/WineDBInterface/Form1.Designer.cs
@@ -29,13 +29,30 @@ namespace WineDBInterfaCe
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // fileSystemWatcher1
+            // 
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            this.fileSystemWatcher1.SynchronizingObject = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+        private System.IO.FileSystemWatcher fileSystemWatcher1;
     }
 }
 
--- WineDBInterface/WineDBInterface/Form1.cs
+++ WineDBInterface/WineDBInterface/Form1.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,10 +14,71 @@ namespace WineDBInterfaCe
 {
     public partial class Form1 : Form
     {
+        private SqlConnection cnn;
+
         public Form1()
         {
             InitializeComponent();
         }
 
+        private SqlConnection getConnection()
+        {
+            return new SqlConnection("data source = tcp:mednat.ieeta.pt\\SQLSERVER,8101; Initial Catalog = p5g8; uid = p5g8; password = AAaa1234");
+        }
+
+        private bool verifyConnection()
+        {
+            if (cnn == null)
+            {
+                cnn = getConnection();
+            }
+
+            if (cnn != null)
+            {
+                try
+                {
+                    if (cnn.State != ConnectionState.Open)
+                    {
+                        cnn.Open();
+                    }
+
+                }
+                catch (SqlException e)
+                {
+                    MessageBox.Show("FAILED TO OPEN CONNECTION TO DATABASE DUE TO THE FOLLOWING ERROR \r\n" + e.Message, "Connection Test", MessageBoxButtons.OK);
+                    return false;
+                }
+
+
+                return cnn.State == ConnectionState.Open;
+            }
+            return false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cnn = getConnection();
+            verifyConnection();
+            DialogResult userfeedback;
+            bool repeat = true;
+            while (repeat)
+            {
+                if (cnn == null || cnn.State != ConnectionState.Open)
+                {
+                    userfeedback = MessageBox.Show("Could not connect to DB. Do tou want to retry?", "ERROR", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error);
+                    repeat = (userfeedback == DialogResult.Retry);
+                    if (userfeedback == DialogResult.Cancel)
+                        this.Close();
+                    verifyConnection();
+                }
+                else
+                {
+                    repeat = false;
+                }
+
+            }
+            //getStats();
+        }
     }
 }
+
