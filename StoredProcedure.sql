51087f702e46346c60b44485656e5ee34f130583
Dani Figueiredo <73801594+daniff15@users.noreply.github.com>
2021-06-16 01:41:46 +0100

    Add files via upload

--- StoredProcedure.sql
+++ StoredProcedure.sql
@@ -19,7 +19,7 @@ BEGIN
 				BEGIN CATCH
 					Rollback TRAN
 					SELECT @erro = ERROR_MESSAGE(); 
-					SET @erro = 'A Pessoa não foi inserida'
+					SET @erro = 'A Pessoa não foi inserida, algum valor inserido incorretamento'
 					RAISERROR (@erro, 16,1);
 				END CATCH
 			END
@@ -52,7 +52,7 @@ AS
 				BEGIN CATCH
 					Rollback TRAN
 					SELECT @erro = ERROR_MESSAGE(); 
-					SET @erro =  'O Cliente não foi inserido'
+					SET @erro =  'O Cliente não foi inserido, algum valor inserido incorretamento'
 					RAISERROR (@erro, 16,1);
 				END CATCH
 			END
@@ -81,7 +81,7 @@ AS
 				BEGIN CATCH
 					Rollback TRAN
 					SELECT @erro = ERROR_MESSAGE(); 
-					SET @erro =  'O Funcionario não foi inserido'
+					SET @erro =  'O Funcionario não foi inserido, algum valor inserido incorretamento'
 					RAISERROR (@erro, 16,1);
 				END CATCH
 			END
@@ -95,26 +95,32 @@ AS
 	BEGIN
 		DECLARE @count INT;
 		DECLARE @erro VARCHAR(100);
+		DECLARE @Num_Funcionario INT;
+
 		SET @count = (SELECT WineDB.checkIfNIFExists(@NIF))
 		IF(@count>=1)
 		
 			RAISERROR ('O NIF introduzido já existe, não é possível adicionar o Gerente', 16,1);
 		ELSE
 		--fazer tambem a verificacao pelo numero de funcionario
-			BEGIN
-				BEGIN TRY
-					BEGIN TRAN
-								EXEC WineDB.AdicionarFuncionario @Nome, @Morada, @NIF, @Data_Nasc, @Genero, @Telemovel, @IBAN, @Num_SS, @Data_Inicio_Atividade;
-								INSERT INTO WineDB.Gerente(NIF, Num_Func) VALUES (@NIF, @Num_Func)
-					COMMIT TRAN
-				END TRY
-				BEGIN CATCH
-					Rollback TRAN
-					SELECT @erro = ERROR_MESSAGE(); 
-					SET @erro = 'O Gerente não foi inserido'
-					RAISERROR (@erro, 16,1);
-				END CATCH
-			END
+			SET @Num_Funcionario = (SELECT WineDB.checkIfNum_FuncExists(@Num_Func))
+			IF(@Num_Funcionario IS NOT NULL)
+				BEGIN
+					BEGIN TRY
+						BEGIN TRAN
+									EXEC WineDB.AdicionarFuncionario @Nome, @Morada, @NIF, @Data_Nasc, @Genero, @Telemovel, @IBAN, @Num_SS, @Data_Inicio_Atividade;
+									INSERT INTO WineDB.Gerente(NIF, Num_Func) VALUES (@NIF, @Num_Func)
+						COMMIT TRAN
+					END TRY
+					BEGIN CATCH
+						Rollback TRAN
+						SELECT @erro = ERROR_MESSAGE(); 
+						SET @erro = 'O Gerente não foi inserido'
+						RAISERROR (@erro, 16,1);
+					END CATCH
+				END
+			ELSE
+				RAISERROR ('O número de funcionário inserido já existe!', 16,1);
 	End
 GO
 
@@ -125,26 +131,32 @@ AS
 	BEGIN
 		DECLARE @count INT;
 		DECLARE @erro VARCHAR(100);
+		DECLARE @Num_Funcionario INT;
+
 		SET @count = (SELECT WineDB.checkIfNIFExists(@NIF))
 		IF(@count>=1)
 		
 			RAISERROR ('O NIF introduzido já existe, não é possível adicionar o Operador Agricola', 16,1);
 		ELSE
 		--fazer tambem a verificacao pelo numero de funcionario
-			BEGIN
-				BEGIN TRY
-					BEGIN TRAN
-								EXEC WineDB.AdicionarFuncionario @Nome, @Morada, @NIF, @Data_Nasc, @Genero, @Telemovel, @IBAN, @Num_SS, @Data_Inicio_Atividade;
-								INSERT INTO WineDB.OperadorAgricola(NIF, Num_Func, ID_Terreno) VALUES (@NIF, @Num_Func, @ID_Terreno)
-					COMMIT TRAN
-				END TRY
-				BEGIN CATCH
-					Rollback TRAN
-					SELECT @erro = ERROR_MESSAGE(); 
-					SET @erro = 'O Operador Agricola não foi inserido'
-					RAISERROR (@erro, 16,1);
-				END CATCH
-			END
+			SET @Num_Funcionario = (SELECT WineDB.checkIfNum_FuncExists(@Num_Func))
+			IF(@Num_Funcionario IS NOT NULL)
+				BEGIN
+					BEGIN TRY
+						BEGIN TRAN
+									EXEC WineDB.AdicionarFuncionario @Nome, @Morada, @NIF, @Data_Nasc, @Genero, @Telemovel, @IBAN, @Num_SS, @Data_Inicio_Atividade;
+									INSERT INTO WineDB.OperadorAgricola(NIF, Num_Func, ID_Terreno) VALUES (@NIF, @Num_Func, @ID_Terreno)
+						COMMIT TRAN
+					END TRY
+					BEGIN CATCH
+						Rollback TRAN
+						SELECT @erro = ERROR_MESSAGE(); 
+						SET @erro = 'O Operador Agricola não foi inserido, algum valor inserido incorretamento'
+						RAISERROR (@erro, 16,1);
+					END CATCH
+				END
+			ELSE
+				RAISERROR ('O número de funcionário inserido já existe!', 16,1);
 	End
 GO
 
@@ -153,27 +165,106 @@ AS
 	BEGIN
 		DECLARE @count INT;
 		DECLARE @erro VARCHAR(100);
+		DECLARE @Num_Funcionario INT;
+
 		SET @count = (SELECT WineDB.checkIfNIFExists(@NIF))
 		IF(@count>=1)
 		
 			RAISERROR ('O NIF introduzido já existe, não é possível adicionar o Operador Adega', 16,1);
 		ELSE
 		--fazer tambem a verificacao pelo numero de funcionario
+			SET @Num_Funcionario = (SELECT WineDB.checkIfNum_FuncExists(@Num_Func))
+			IF(@Num_Funcionario IS NOT NULL)
+				BEGIN
+					BEGIN TRY
+						BEGIN TRAN
+								EXEC WineDB.AdicionarFuncionario @Nome, @Morada, @NIF, @Data_Nasc, @Genero, @Telemovel, @IBAN, @Num_SS, @Data_Inicio_Atividade;
+								INSERT INTO WineDB.OperadorAdega(NIF, Num_Func, ID_Adega) VALUES (@NIF, @Num_Func, @ID_Adega)
+						COMMIT TRAN
+					END TRY
+					BEGIN CATCH
+						Rollback TRAN
+						SELECT @erro = ERROR_MESSAGE(); 
+						SET @erro = 'O Operador Adega não foi inserido, algum valor inserido incorretamento'
+						RAISERROR (@erro, 16,1);
+					END CATCH
+				END
+			ELSE
+				RAISERROR ('O número de funcionário inserido já existe!', 16,1);
+	
+	END
+GO
+
+EXEC WineDB.AdicionarOperadorAdega 'Maria Albertina', 'Lisboa', 123444344, '2001-12-12', 'M', '765839457', PT12345678909876543212345, 234567, '2002-12-12', 17, 'CCCCC'
+
+SELECT * FROM WineDB.Funcionario
+
+CREATE PROCEDURE WineDB.AdicionarVinho (@ID VARCHAR(5), @ID_Cuba INT, @Nome VARCHAR(256), @DOC VARCHAR(32), @ID_Casta INT)
+AS
+	BEGIN
+		DECLARE @count INT;
+		DECLARE @erro VARCHAR(100);
+		SET @count = (SELECT WineDB.checkIfWineExists(@ID))
+		IF(@count>=1)
+			RAISERROR ('O vinho introduzido já existe, não é possível adicioná-lo', 16,1);
+		ELSE
 			BEGIN
 				BEGIN TRY
 					BEGIN TRAN
-								EXEC WineDB.AdicionarFuncionario @Nome, @Morada, @NIF, @Data_Nasc, @Genero, @Telemovel, @IBAN, @Num_SS, @Data_Inicio_Atividade;
-								INSERT INTO WineDB.OperadorAdega(NIF, Num_Func, ID_Adega) VALUES (@NIF, @Num_Func, @ID_Adega)
+								INSERT INTO WineDB.Vinho(ID, ID_Cuba, Nome, DOC, ID_Casta) VALUES (@ID, @ID_Cuba, @Nome, @DOC, @ID_Casta)
 					COMMIT TRAN
 				END TRY
 				BEGIN CATCH
 					Rollback TRAN
 					SELECT @erro = ERROR_MESSAGE(); 
-					SET @erro = 'O Operador Adega não foi inserido'
+					SET @erro = 'Vinho não foi inserido, algum valor inserido incorretamento'
 					RAISERROR (@erro, 16,1);
 				END CATCH
 			END
 	End
 GO
 
+--EXEC WineDB.AdicionarVinho 'CCCCC', 12452, 'Vinho da Nossa Senhora', 'Douro' , 12452
 
+CREATE PROCEDURE WineDB.AdicionarAdega (@ID VARCHAR(5), @Nome VARCHAR(256), @Endereco VARCHAR(256), @Cap_Max INT, @Num_Cubas INT, @NIF_Gerente INT)
+AS
+	BEGIN
+		DECLARE @count INT;
+		DECLARE @erro VARCHAR(100);
+		SET @count = (SELECT WineDB.checkIfAdegaExists(@ID))
+		IF(@count>=1)
+			RAISERROR ('A adega introduzida já existe, não é possível adicioná-la', 16,1);
+		ELSE
+			BEGIN
+				BEGIN TRY
+					BEGIN TRAN
+						INSERT INTO WineDB.Adega(ID, Nome, Endereco, Cap_Max, Num_Cubas, NIF_Gerente) VALUES (@ID, @Nome, @Endereco, @Cap_Max, @Num_Cubas, @NIF_Gerente)
+					COMMIT TRAN
+				END TRY
+				BEGIN CATCH
+					Rollback TRAN
+					SELECT @erro = ERROR_MESSAGE(); 
+					SET @erro = 'Adega não foi inserida, algum valor inserido incorretamento'
+					RAISERROR (@erro, 16,1);
+				END CATCH
+			END
+	End
+GO
+
+--EXEC WineDB.AdicionarAdega 'CCCCC', 'Adeguinha', 'Rua do Sacramento, Setúbal', 300.0 , 30, 237489547
+
+--CREATE PROCEDURE WineDB.Testinho(@Num_Func INT)
+--AS
+--	BEGIN
+
+--	DECLARE @NUMERO INT
+
+--	SET @NUMERO = (SELECT WineDB.Teste(@Num_Func))
+
+--	IF @NUMERO = 227578689
+--		SELECT * FROM WineDB.Adega
+
+--	END
+--GO
+
+--EXEC WineDB.Testinho 13
--- UDP.sql
+++ UDP.sql
@@ -13,9 +13,102 @@ GO
 CREATE FUNCTION WineDB.checkIfNum_FuncExists (@Num_Func INT) RETURNS INT
 AS
 	BEGIN
+		DECLARE @NIFPessoa INT
 		DECLARE @counter INT
-		SELECT @counter=COUNT(1) FROM WineDB.Pessoa WHERE NIF=@NIF
-		--VER ISTO PARA VERIICAR NOS 3 TIPOS DE FUNCIONARIO
+		DECLARE @counterPessoa INT
+		DECLARE @counterGerente INT
+		DECLARE @counterOpAdega INT
+		DECLARE @counterOpAgricola INT
+
+		--Check if Pessoa exists
+		SELECT @NIFPessoa = WineDB.getNIFFuncfromNum_Func(@Num_Func)
+
+		IF @NIFPessoa IS NOT NULL
+			SELECT @counterPessoa=(SELECT WineDB.checkIfNIFExists(@NIFPessoa))
+		
+		IF(@counterPessoa IS NOT NULL)
+			--Check if gerente exists
+			SELECT @counterGerente = COUNT(1) FROM WineDB.Gerente WHERE Num_Func = @Num_Func
+			--Check if Operador Adega exists
+			SELECT @counterOpAdega = COUNT(1) FROM WineDB.OperadorAdega WHERE Num_Func = @Num_Func
+			--Check if Operador Agricola exists
+			SELECT @counterOpAgricola = COUNT(1) FROM WineDB.OperadorAgricola WHERE Num_Func = @Num_Func
+
+		IF(@counterGerente = 1 OR @counterOpAdega = 1 OR @counterOpAgricola = 1)
+			SET @counter = 1
+		ELSE
+			SET @counter = 0
+
 		RETURN @counter
 	END
 GO
+
+CREATE FUNCTION WineDB.checkIfWineExists (@ID VARCHAR(5)) RETURNS INT
+AS
+	BEGIN
+		DECLARE @counter INT
+		SELECT @counter=COUNT(*) FROM WineDB.Vinho WHERE ID = @ID
+		RETURN @counter
+	END
+GO
+
+CREATE FUNCTION WineDB.checkIfAdegaExists (@ID VARCHAR(5)) RETURNS INT
+AS
+	BEGIN
+		DECLARE @counter INT
+		SELECT @counter=COUNT(*) FROM WineDB.Adega WHERE ID = @ID
+		RETURN @counter
+	END
+GO
+
+CREATE FUNCTION WineDB.getNIFFuncfromNum_Func(@Num_Func INT) RETURNS INT
+AS
+	BEGIN	
+		DECLARE @condition INT
+		DECLARE @NIFFunc INT
+		DECLARE @counter INT
+		DECLARE @counterGerente INT
+		DECLARE @counterOpAdega INT
+		DECLARE @counterOpAgricola INT
+
+		SELECT @counterGerente = COUNT(1) FROM WineDB.Gerente WHERE Num_Func = @Num_Func
+			--Check if Operador Adega exists
+		SELECT @counterOpAdega = COUNT(1) FROM WineDB.OperadorAdega WHERE Num_Func = @Num_Func
+			--Check if Operador Agricola exists
+		SELECT @counterOpAgricola = COUNT(1) FROM WineDB.OperadorAgricola WHERE Num_Func = @Num_Func
+
+		IF @counterGerente = 1
+			-- nao sei, se nao encontrar, se da erro ou nao
+			-- SUPONDO Q O NUM_FUNC JA TA COMO UNIQUE
+			SET @condition = @counterGerente
+			IF @condition = 1
+				SET @NIFFUNC = (SELECT NIF FROM WineDB.Gerente WHERE Num_Func = @Num_Func)
+		IF @counterOpAdega = 1
+			-- nao sei, se nao encontrar, se da erro ou nao
+			-- SUPONDO Q O NUM_FUNC JA TA COMO UNIQUE
+			SET @condition = @counterOpAdega
+			IF @condition = 1
+				SET @NIFFUNC = (SELECT NIF FROM WineDB.OperadorAdega WHERE Num_Func = @Num_Func)
+		IF @counterOpAgricola = 1
+			-- nao sei, se nao encontrar, se da erro ou nao
+			-- SUPONDO Q O NUM_FUNC JA TA COMO UNIQUE
+			SET @condition = @counterOpAgricola
+			IF @condition = 1
+				SET @NIFFUNC = (SELECT NIF FROM WineDB.OperadorAgricola WHERE Num_Func = @Num_Func)
+
+		RETURN @NIFFunc --means that there is no one with this employee number
+	END
+GO
+
+--CREATE FUNCTION WineDB.Teste(@Numero INT) RETURNS INT
+--AS
+	--BEGIN
+		--DECLARE @numerozinho INT
+
+			--SET @numerozinho = (SELECT NIF FROM WineDB.OperadorAgricola WHERE Num_Func = @Numero)
+
+		--RETURN @numerozinho
+	--END
+--GO
+
+SELECT NIF FROM WineDB.OperadorAgricola WHERE Num_Func = 13
